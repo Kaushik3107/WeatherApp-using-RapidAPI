bffd5b23a5d6333f6f02e5e454906cec967633e3
Kaushik Dange <kaushikdange3107@gmail.com>
2024-10-09 16:37:21 +0530

    Initial Commit

---
+++ src/app/Environment/EnvironmentVariables.ts
@@ -0,0 +1,34 @@
+// export const EnvironmentalVariables = {
+//     production:false,
+
+//     //Base Urls to access the end points using the http client get method
+//     weatherApiLocationBaseURL:'https://weather338.p.rapidapi.com/locations/search?',
+//     weatherApiForecastBaseURL:'https://weather338.p.rapidapi.com/weather/forecast?',
+
+//     //Variables for API key name and value
+//     xRapidApiKeyName:'X-RapidAPI-Key',
+//     xRapidApikeyValue:'913e57de3dmsh90022ec7950ea41p18b12fjsne4d11d08ab13',
+
+//     //Variables for host name and value
+//     xRapidApiHostName:'X-RapidAPI-Host',
+//     xRapidApiHostValue:'weather338.p.rapidapi.com'
+
+// }
+
+export const EnvironmentalVariables = {
+  production: false,
+
+  //Base Urls to access the end points using the http client get method
+  weatherApiLocationBaseURL:
+    'https://weather338.p.rapidapi.com/locations/search?',
+  weatherApiForecastBaseURL:
+    'https://weather338.p.rapidapi.com/weather/forecast?',
+
+  //Variables for API key name and value
+  xRapidApiKeyName: 'x-rapidapi-key',
+  xRapidApikeyValue: 'b7952e2d5emshe0ef5bcb2d8afefp1ff19ajsna2d67dbf56b1',
+
+  //Variables for host name and value
+  xRapidApiHostName: 'x-rapidapi-host',
+  xRapidApiHostValue: 'weather338.p.rapidapi.com',
+};
